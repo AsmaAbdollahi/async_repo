eaa3c390c52c5a6971a6104ac8659c64260a91c8
asi <you@example.com>
2025-08-18 23:45:04 +0330

    writer.py file changed to async form

--- writer.py
+++ writer.py
@@ -1,10 +1,14 @@
+import aiofiles
+
 class Writer:
     def __init__(self, filename):
         self.filename = filename
 
-    def write(self, results):
-        with open(self.filename, "w", encoding="utf-8") as f:
+    async def write(self, results):
+        # فایل رو به صورت async باز می‌کنیم
+        async with aiofiles.open(self.filename, "w", encoding="utf-8") as f:
             for url, content in results:
-                f.write(f"===== {url} =====\n")
-                f.write(content[:500])  # فقط ۵۰۰ کاراکتر اول برای کوتاهی
-                f.write("\n\n")
+                # هر بار async بنویس
+                await f.write(f"===== {url} =====\n")
+                await f.write(content[:500])  # فقط ۵۰۰ کاراکتر اول برای کوتاهی
+                await f.write("\n\n")
