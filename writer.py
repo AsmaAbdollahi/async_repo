f938e34e5d0094fe947cd9ae6ac18e0f06bb57e6
AsmaAbdollahi <154994504+AsmaAbdollahi@users.noreply.github.com>
2025-08-22 12:37:17 +0000

    delete persian comments

--- writer.py
+++ writer.py
@@ -5,10 +5,10 @@ class Writer:
         self.filename = filename
 
     async def write(self, results):
-        # فایل رو به صورت async باز می‌کنیم
+       
         async with aiofiles.open(self.filename, "w", encoding="utf-8") as f:
             for url, content in results:
-                # هر بار async بنویس
+            
                 await f.write(f"===== {url} =====\n")
                 await f.write(content[:500])  # فقط ۵۰۰ کاراکتر اول برای کوتاهی
                 await f.write("\n\n")
